a882ee0ef3826696673c9060e8291a0b28a6f708
dimitrimike <dimitrimike26@gmail.com>
2026-08-26 11:44:46 -0300

    Atualização 4

--- WORKSPACE Estrutura de Dados/DOCUMENTACAO.md
+++ WORKSPACE Estrutura de Dados/DOCUMENTACAO.md
@@ -6,3 +6,5 @@
 4. ``python manage.py startapp nome_do_app`` -> Criando um novo app.
 5. ``python manage.py migrate`` -> Realiza as migrações(ativa) do projeto.
 6. ``python magane.py createsuperuser`` -> Criar um novo super usuário.
+7. ``python manage.py changepassword nomedousuario`` -> Altera a senha, caso você esqueça.
+8. ``python manage.py makemigrations`` -> Cria/gera novo pacote de migração (Útil para novo app e alteração do BD).cls
--- WORKSPACE Estrutura de Dados/sistema/models.py
+++ WORKSPACE Estrutura de Dados/sistema/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db import models # ORM do Django para declarar modelos no banco de dados.
+from django.utils import timezone # Útil para gerar data/hota atual no projeto.
 
-# Create your models here.
+# Modelo que representa um Paciente.
+# atributos => nome, sobrenome, email, telefone, data de cadastro, mensagem, ativo(True/False).
+class Paciente(models.Model):
+    nome = models.CharField(max_length=25) # Nome do paciente.
+    sobrenome = models.CharField(max_length=25) # Sobrenome do paciente.
+    email = models.EmailField() # Email de contato de paciente.
+    telefone = models.CharField(max_length=20) # Telefone de contato do paciente.
+    criacao_data = models.DateTimeField(default=timezone.now) # Data/hora do cadastro.
+    mensagem = models.TextField(blank=True) # Campo opcional livre oara mensagem.
+    ativo = models.BooleanField(default=True) 
